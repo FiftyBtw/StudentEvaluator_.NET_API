decf423760086d6cf2bef796169597e94b477de4
bepaczkows <Benjamin.PACZKOWSKI@etu.uca.fr>
2024-03-12 14:41:57 +0100

    Séparations des TU

--- StudentEvaluator_API_EF/UnitTests/Entity/EStudentTests.cs
+++ StudentEvaluator_API_EF/UnitTests/Entity/EStudentTests.cs
@@ -5,79 +5,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EF_UnitTests.Entity;
 
-public class EStudentxGroupTests
+public class EStudentTests
 {
-    [Fact]
-    public void TestAddGroup()
-    {
-        // Arrange
-        var connection = new SqliteConnection("DataSource=:memory:");
-        connection.Open();
-
-        var options = new DbContextOptionsBuilder<LibraryContext>()
-            .UseSqlite(connection)
-            .Options;
-
-        var groupToAdd = new GroupEntity
-        {
-            GroupYear = 1,
-            GroupNumber = 1
-        };
-
-        // Act
-        using (var context = new LibraryContext(options))
-        {
-            context.Database.EnsureCreated();
-
-            context.GroupSet.Add(groupToAdd);
-            context.SaveChanges();
-
-            // Assert
-            var groupFromDb = context.GroupSet.FirstOrDefault();
-            Assert.NotNull(groupFromDb);
-
-            Assert.Equal(groupToAdd.GroupYear, groupFromDb.GroupYear);
-            Assert.Equal(groupToAdd.GroupNumber, groupFromDb.GroupNumber);
-        }
-    }
-
-    
-    [Fact]
-    public void TestDeleteGroup()
-    {
-        // Arrange
-        var connection = new SqliteConnection("DataSource=:memory:");
-        connection.Open();
-
-        var options = new DbContextOptionsBuilder<LibraryContext>()
-            .UseSqlite(connection)
-            .Options;
-
-        var groupToAdd = new GroupEntity
-        {
-            GroupYear = 1,
-            GroupNumber = 1
-        };
-
-        // Act
-        using (var context = new LibraryContext(options))
-        {
-            context.Database.EnsureCreated();
-
-            context.GroupSet.Add(groupToAdd);
-            context.SaveChanges();
-
-            context.GroupSet.Remove(groupToAdd);
-            context.SaveChanges();
-
-            // Assert
-            var groupFromDb = context.GroupSet.FirstOrDefault();
-            Assert.Null(groupFromDb);
-        }
-    }
-    
-    
-  [Fact]
+     [Fact]
     public void TestAddStudent()
     {
         // Arrange
